310e6d949093f47e51d593241f000dbf4616a181
ElizabethCowell <80732868+ElizabethCowell@users.noreply.github.com>
2021-04-20 13:00:10 -0400

    First Factory Pattern Design

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FactoryPatternExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many tires does your vehicle have?");
+
+            var response = Console.ReadLine();
+
+            var drive = VehicleFactory.VehicleType(response);
+
+            drive.Drive();
+        }
+    }
+}
---
+++ VehicleFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise
+{
+    static class VehicleFactory
+    {
+        public static IVehicle VehicleType(string numberOfTires)
+        {
+            switch (numberOfTires)
+            {
+                case "4":
+                case "four":
+                    return new Car();
+                case "2":
+                case "two":
+                    return new Motorcycle();
+                default:
+                    return new Empty();
+            }
+        }
+    }
+}
